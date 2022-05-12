0bfc731159af6c5d6ffe8f78861ae1c74e26417c
devleon <devleon00@gmail.com>
2022-05-12 13:16:37 -0500

    Agregando test de clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Test de clase Require", () => {
+  test("1) Verificando en largo de lineas", () => {
+    const result = Reader.readJsonFile("data/explorers.json");
+    expect(result.length).toBe(15)
+  });
+});
