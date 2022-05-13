cc57283e1319df2c556dc15e60f7127d5982bcf2
devleon <devleon00@gmail.com>
2022-05-13 08:23:38 -0500

    Agregando requerimiento 4

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,42 +2,71 @@ const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe("Test de metodos de clase ExplorerService", () => {
     test("1) Probando 'filterByMission()'", () => {
-        const explorer = [{
-            name: "Woopa2",
-            githubUsername: "ajolonauta2",
-            score: 2,
-            mission: "node",
-            stacks: ["javascript", "groovy", "elm"],
-        }];
+        const explorer = [
+            {
+                name: "Woopa2",
+                githubUsername: "ajolonauta2",
+                score: 2,
+                mission: "node",
+                stacks: ["javascript", "groovy", "elm"],
+            },
+        ];
 
         const result = ExplorerService.filterByMission(explorer, "node");
         expect(result.length).toBe(1);
     });
 
     test("2) Probando 'getAmountOfExplorersByMission()'", () => {
-        const explorer = [{
-            name: "Woopa2",
-            githubUsername: "ajolonauta2",
-            score: 2,
-            mission: "node",
-            stacks: ["javascript", "groovy", "elm"],
-        }];
+        const explorer = [
+            {
+                name: "Woopa2",
+                githubUsername: "ajolonauta2",
+                score: 2,
+                mission: "node",
+                stacks: ["javascript", "groovy", "elm"],
+            },
+        ];
 
-        const result = ExplorerService.getAmountOfExplorersByMission(explorer, "node");
+        const result = ExplorerService.getAmountOfExplorersByMission(
+            explorer,
+            "node"
+        );
         expect(result).toBe(1);
     });
 
     test("3) Probando 'getExplorersUsernamesByMission()'", () => {
-        const explorer = [{
-            name: "Woopa2",
-            githubUsername: "ajolonauta2",
-            score: 2,
-            mission: "node",
-            stacks: ["javascript", "groovy", "elm"],
-        }];
+        const explorer = [
+            {
+                name: "Woopa2",
+                githubUsername: "ajolonauta2",
+                score: 2,
+                mission: "node",
+                stacks: ["javascript", "groovy", "elm"],
+            },
+        ];
 
-        const result = ExplorerService.getExplorersUsernamesByMission(explorer, "node");
+        const result = ExplorerService.getExplorersUsernamesByMission(
+            explorer,
+            "node"
+        );
         expect(result[0]).toBe("ajolonauta2");
     });
-});
 
+    test("4) Probando 'getExplorersByStack()'", () => {
+        const explorer = [
+            {
+                name: "Woopa2",
+                githubUsername: "ajolonauta2",
+                score: 2,
+                mission: "node",
+                stacks: ["javascript", "groovy", "elm"],
+            },
+        ];
+
+        const result = ExplorerService.getExplorersByStack(
+            explorer,
+            "javascript"
+        );
+        expect(result).toEqual(explorer);
+    });
+});
